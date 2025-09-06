51123d1976a2f28a5bbe61b60461c2a4d0a291f6
Pawan Prajapati <138299245+pawanpr527@users.noreply.github.com>
2025-09-07 00:17:42 +0530

    Create BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,10 @@
+def BubbleSort(arr):
+    left = 0
+    for i in range(0,len(arr)-1):
+        for j in range(len(arr)-1-i):
+            if arr[j]>arr[j+1]:
+                arr[j+1],arr[j] = arr[j],arr[j+1] 
+        print(arr)
+    return arr
+arr = [5,2,4,6,2,9,8]
+print(BubbleSort(arr))
